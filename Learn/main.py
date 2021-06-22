e3b1a14b1cd4c4c45d0cc3719514e98a6bf72dcf
Kylotrain <tomy_ramdiz@outlook.com>
2021-06-22 00:45:22 -0500

    Before embed a layout

--- Learn/main.py
+++ Learn/main.py
@@ -1,5 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class BLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        b1 = Button(text="H")
+        b2 = Button(text="H2")
+        self.add_widget(b1)
+        self.add_widget(b2)
 
 class MainWidget(Widget):
     pass
